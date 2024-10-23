ec1c3b9688aa65ecaa9d44996c5842439ef64f7c
Nakul Koshal <nakulkoshal4@gmail.com>
2024-10-24 01:37:50 +0530

    Update Synchronous_FIFO.v

--- Synchronous_FIFO.v
+++ Synchronous_FIFO.v
@@ -1,23 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 29.09.2024 03:15:55
-// Design Name: 
-// Module Name: Synchronous_FIFO
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+
 
 
 module Synchronous_FIFO
